190b07ecef550c9fe7a4e18f7b93786d13169d29
Patrick <the4dpatrick@yahoo.com>
2013-08-16 18:32:48 +0900

    part 1

--- space_shooter.js
+++ space_shooter.js
@@ -1,4 +1,12 @@
+var game = new Game();
+
+function init() {
+	if(game.init())
+		game.start();
+}
+
 var imageRepository = new function() {
+	this.empty = null;
 	this.background = new Image();
 	
 	this.background.src = "imgs/bg.png";
@@ -27,8 +35,48 @@ function Background() {
 		
 		this.context.drawImage(imageRepository.background, this.x, this.y - this.canvasHeight);
 		
-		if (this.y &gt;= this.canvasHeight)
+		if (this.y >= this.canvasHeight)
 			this.y = 0;
 	};
 }
 Background.prototype = new Drawable();
+
+function Game() {
+	this.init = function() {
+		this.bgCanvas = document.getElementById('background');
+		
+		if (this.bgCanvas.getContext) {
+			this.bgContext = this.bgCanvas.getContext('2d');
+			
+			Background.prototype.context = this.bgContext;
+			Background.prototype.canvasWidth = this.bgCanvas.width;
+			Background.prototype.canvasHeight = this.bgCanvas.height;
+			
+			this.background = new Background();
+			this.background.init(0,0);
+			return true;
+		} else {
+			return false;
+		}
+	};
+	this.start = function() {
+		animate();
+	};
+}
+
+function animate() {
+	requestAnimFrame( animate );
+	game.background.draw();
+}
+
+window.requestAnimFrame = (function(){
+	return window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function( callback, element){
+			window.setTimeout(callback, 1000 / 60); //60 frames per second.
+		};
+})();
+
